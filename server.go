0b781d224db1875896c922520f9961e70465603e
Arian <arian@web-essentials.co>
2025-11-27 17:09:10 +0700

    [TASK] My first API with GO
    refs LETSGO-0

--- server.go
+++ server.go
@@ -1,16 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+type User struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+var users []User
+
 func main() {
-	http.HandleFunc("/", handler)
+	router := mux.NewRouter()
+	router.HandleFunc("/", handler)
+	router.HandleFunc("/users", getUsers).Methods("GET")
+	router.HandleFunc("/users", createUsers).Methods("POST")
+
+	// http.HandleFunc("/", handler)
 	port := ":8080"
 	fmt.Println("Server is running on", "http://localhost"+port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	// log.Fatal(http.ListenAndServe(port, nil))
+	log.Fatal(http.ListenAndServe(port, router))
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(users)
+}
+
+func createUsers(w http.ResponseWriter, r *http.Request) {
+	var newUser User
+	_ = json.NewDecoder(r.Body).Decode(&newUser)
+	users = append(users, newUser)
+	json.NewEncoder(w).Encode(newUser)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
